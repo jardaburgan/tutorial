ecff9c7973832e7cb0851387bde6d135aeceacfa
jardaburgan <jardaburgan@gmail.com>
2023-11-19 20:57:09 +0100

    přidal jsem Hello world

--- src/Hangman.py
+++ src/Hangman.py
@@ -14,17 +14,29 @@ for one_letter in hidden_word:
     printedWord += one_letter
 print(printedWord)
 
+# Životy
+lives = 6
+
 # Hádání písmene a pokud jsem uhádl, tak dosadím místo podtržítka
 while "_" in hidden_word:
     guess = input("Zadejte hádané písmeno\n").lower()
     for index in range(0,len(random_resort)):
         if guess == random_resort[index]:
             hidden_word[index] = guess
+
+# Počítání a kontrola životů
+    if guess not in random_resort:
+        lives -= 1
+    print(f"Počet Vašich životů je {lives}.")
+    if lives == 0:
+        print("Prohrál jsi!!!")
+
 # Vypsání zkrytého slova v normální podobě a ne jako list
-    printedWord = ""
-    for one_letter in hidden_word:
-        printedWord +=one_letter
-    print(printedWord)
+printedWord = ""
+for one_letter in hidden_word:
+    printedWord +=one_letter
+print(printedWord)
+
 # Kontrola vítezství
 if "_" not in hidden_word:
     print("Vyhrál jsi!!!")
