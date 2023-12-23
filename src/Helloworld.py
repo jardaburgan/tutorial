3b6e8fe52f0c11acbd1123a0a8464a191931ffd6
jardaburgan <jardaburgan@gmail.com>
2023-12-23 18:28:50 +0100

    přidal jsem Hello world

--- src/Helloworld.py
+++ src/Helloworld.py
@@ -1,9 +1,9 @@
-# SPOJOVÁNÍ ŘETEZCŮ
+# # 1. SPOJOVÁNÍ ŘETEZCŮ
 # vstup = input("Zadej něco, co chceš opakovat\n")
 # vystup = vstup +" "+ vstup
 # print(vystup)
 
-# ZAOKROUHLOVÁNÍ
+# # 2. ZAOKROUHLOVÁNÍ
 # cislo_a = float(input("Zadej číslo a"))
 # cislo_b = float(input("Zadej číslo b"))
 # result = cislo_a * cislo_b
@@ -11,3 +11,72 @@
 # print(round(result,0))
 # print(round(result,-1))
 
+# # 3. MATEMATICKÉ OPERÁTORY
+# x = 10
+# y = 2
+# # sčítání
+# print(x + y)
+# # odčítání
+# print(x - y)
+# # násobení
+# print(x * y)
+# # dělení
+# print(x/y)
+# # mocnění
+# print(x**y)
+# # modulo (zbytek při dělení)
+# print(x%y)
+
+# # 4. MODULY
+# # decimal - datový typ desetiných čísel
+# # fractions - modul na zlomky
+
+# # 5. DATOVÉ TYPY
+# # text: str
+# # celé číslo: int
+# # desetinné číslo: float, decimal z modulu decimal
+# # pravda nebo nepravda: bool
+# # zlomky: Fraction z modulu franctions
+# from fractions import Fraction
+# a = Fraction (3,4)
+# b = Fraction (1,7)
+# print(a+b)
+# # funkce zobrazující datový typ: type
+
+# # 6. TEXTOVÉ ŘETĚZCE
+# prázdný_řetězec = " "
+# prázdný_řetězec = ' '
+# prázdný_řetězec = ''' '''
+# #délka řetezce - len
+# a = "jaroslav"
+# print(len(a))
+# # replikování (opakování) řetězců
+# retezec = "Bum! "
+# print(retezec * 7)
+# # string.startswith(value, start, end): Vrátí TRUE, pokud string začíná definovaným textem
+# vstup = "jaroslavek"
+# print(vstup.startswith("jar"))
+# print(vstup.startswith("ar",1,3))
+# # string.endswith(value, start, end): Vrátí TRUE, pokud string končí definovaným textem
+# # in: Ověření, zda se část textu nachází v řetezci
+# vstup = "jaroslavek"
+# print("osla" in vstup)
+# # string.lower(): přehodí všechny znaky v řetězci na malá písmenka
+# # string.upper(): přehodí všechny znaky v řetězci na velká písmenka
+# name = "JARoslavek"
+# print(name.upper())
+# print(name.lower())
+# # string.strip(): odstraní veškeré mezery v řetězci
+# # string.replace(oldvalue, newvalue, count): nahradí část řetězce za jiný
+# a = "Python je špatný"
+# print(a.replace("špatný","dobrý"))
+
+# # 7. PODMÍNKY A VĚTVENÍ
+# a = 5
+# b = 6
+# if a > b:
+#     print("Je to ok")
+# elif a == b:
+#     print ("OK")
+# else: print("Dobrý")
+# #
